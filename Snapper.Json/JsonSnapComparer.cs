5b65e9c0d65e9bfb46131b6759b6adf5960c6bcd
Simar Kalra <simarkalra1993@gmail.com>
2018-02-10 16:21:09 +1300

    Initial commit

---
+++ Snapper.Json/JsonSnapComparer.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using Snapper.Core;
+
+namespace Snapper.Json
+{
+    public class JsonSnapComparer : ISnapComparer
+    {
+        public bool Compare(object oldSnap, object newSnap)
+        {
+            var old = JToken.FromObject(oldSnap);
+            var @new = JToken.FromObject(newSnap);
+            return JToken.DeepEquals(old, @new);
+        }
+    }
+}
---
+++ Snapper.Json/JsonSnapper.cs
@@ -0,0 +1,16 @@
+﻿using Snapper.Core;
+
+namespace Snapper.Json
+{
+    public class JsonSnapper : SnapperCore
+    {
+        public JsonSnapper(IAssert asserter, string snapshotDirectory)
+            : base(asserter, new JsonSnapStore(), new StaticPathResolver(snapshotDirectory), new EnvironmentVariableUpdateDecider(),
+                new JsonSnapComparer())
+        {}
+
+        protected JsonSnapper(IAssert asserter, ISnapStore store, IPathResolver resolver, ISnapUpdateDecider snapUpdateDecider, ISnapComparer comparer)
+            : base(asserter, store, resolver, snapUpdateDecider, comparer)
+        {}
+    }
+}

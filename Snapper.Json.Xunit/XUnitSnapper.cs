4077c8be46586f71b5236e2b58388a687a02f04c
Simar Kalra <simarkalra1993@gmail.com>
2018-02-11 00:08:40 +1300

    Extend Xunit assert

--- Snapper.Json.Xunit/XUnitSnapper.cs
+++ Snapper.Json.Xunit/XUnitSnapper.cs
@@ -4,10 +4,7 @@ namespace Snapper.Json.Xunit
 {
     public class XUnitSnapper : JsonSnapper
     {
-        public static void Snap(object value)
-            => Create().SnapObject(value);
-
-        private static XUnitSnapper Create()
+        internal static XUnitSnapper Create()
             => new XUnitSnapper(new XUnitAsserter(), new JsonSnapStore(), new XUnitPathResolver(), new EnvironmentVariableUpdateDecider(), new JsonSnapComparer());
 
         protected XUnitSnapper(IAssert asserter, ISnapStore store, IPathResolver resolver, ISnapUpdateDecider snapUpdateDecider, ISnapComparer comparer) 
@@ -15,7 +12,7 @@ namespace Snapper.Json.Xunit
         {
         }
 
-        private void SnapObject(object value)
+        internal void SnapObject(object value)
             => Snap(string.Empty, value);
     }
 }

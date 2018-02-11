023aff238625241934bdbedba91cb4428311baf9
Simar <theramis@users.noreply.github.com>
2018-02-11 14:10:31 +1300

    Follow same pattern for initialising Snapper as others

--- Snapper.Core/Snapper.cs
+++ Snapper.Core/Snapper.cs
@@ -2,9 +2,11 @@
 {
     public class Snapper : SnapperCore
     {
-        public static Snapper Create(IAssert asserter, string snapshotDirectory)
-            => new Snapper(asserter, new ByteSnapStore(), new StaticPathResolver(snapshotDirectory), new EnvironmentVariableUpdateDecider(),
-                new DefaultSnapComparer());
+        public Snapper(IAssert asserter, string snapshotDirectory)
+            : base(asserter, new ByteSnapStore(), new StaticPathResolver(snapshotDirectory), new EnvironmentVariableUpdateDecider(),
+                new DefaultSnapComparer())
+        {
+        }
 
         private Snapper(IAssert asserter, ISnapStore store, IPathResolver resolver, ISnapUpdateDecider snapUpdateDecider, ISnapComparer comparer)
             : base(asserter, store, resolver, snapUpdateDecider, comparer)

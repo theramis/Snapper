eae0e7d42890dc37e5456375cb2aca1918e95e8b
Simar Kalra <simarkalra1993@gmail.com>
2018-02-14 17:40:52 +1300

    convert object to string before comparing

--- Snapper.Json/JsonSnapComparer.cs
+++ Snapper.Json/JsonSnapComparer.cs
@@ -8,7 +8,9 @@ namespace Snapper.Json
         public bool Compare(object oldSnap, object newSnap)
         {
             var old = JToken.FromObject(oldSnap);
+
             var @new = JToken.FromObject(newSnap);
+            @new = JObject.Parse(@new.ToString());
             return JToken.DeepEquals(old, @new);
         }
     }

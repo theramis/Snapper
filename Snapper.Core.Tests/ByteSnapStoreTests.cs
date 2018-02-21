c2b504f7cc5ccb862af3bd14232be43e5119d261
Simar <theramis@users.noreply.github.com>
2018-02-21 23:26:23 +1300

    Add all tests for Snapper.Core.Tests

---
+++ Snapper.Core.Tests/ByteSnapStoreTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Snapper.Core.Tests
+{
+    public class ByteSnapStoreTests
+    {
+        [Fact]
+        public void ObjectToString_StringToObject()
+        {
+            var obj = new DummyObject
+            {
+                Int = 10,
+                IntList = new List<int> { 21, 22 },
+                String = "randomstring"
+            };
+
+            var objString = ByteSnapStore.ObjectToString(obj);
+            var newObj = ByteSnapStore.StringToObject(objString);
+
+            var dummyObj = newObj as DummyObject;
+            
+            Assert.Equal(obj.String, dummyObj?.String);
+            Assert.Equal(obj.Int, dummyObj?.Int);
+            Assert.Equal(obj.IntList.Count, dummyObj?.IntList.Count);
+
+            for (var i = 0; i < obj.IntList.Count ; i++)
+            {
+                Assert.Equal(obj.IntList[i], dummyObj?.IntList[i]);
+            }
+        }
+
+        [Serializable]
+        private class DummyObject
+        {
+            public string String { get; set; }
+            public int Int { get; set; }
+            public List<int> IntList { get; set; }
+        }
+    }
+}

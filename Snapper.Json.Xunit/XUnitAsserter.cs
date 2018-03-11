6a13a562615de00120df7ea96ac496ad0f2c68be
Simar Kalra <TheRamis@users.noreply.github.com>
2018-03-11 23:38:27 +1300

    Add diff to xunit asserter

--- Snapper.Json.Xunit/XUnitAsserter.cs
+++ Snapper.Json.Xunit/XUnitAsserter.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using DiffMatchPatch;
+using Newtonsoft.Json.Linq;
 using Snapper.Core;
 using Xunit;
 
@@ -20,7 +24,78 @@ namespace Snapper.Json.Xunit
         {
             var old = JToken.FromObject(oldValue);
             var @new = JToken.FromObject(newValue);
-            Assert.True(false, $"Expected snapshot with \n{old}\n but found \n{@new}");
+            var diff = GetDiffMessage(old, @new);
+            Assert.True(false, $"Received value does not match stored snapshot.\n {diff}");
+        }
+
+        private static string GetDiffMessage(JToken old, JToken @new)
+        {
+            var dmp = new diff_match_patch();
+            var a = dmp.diff_linesToChars(old.ToString(), @new.ToString());
+            var lineText1 = (string) a[0];
+            var lineText2 = (string) a[1];
+            var lineArray = (List<string>) a[2];
+            var diffs = dmp.diff_main(lineText1, lineText2, false);
+            dmp.diff_charsToLines(diffs, lineArray);
+            dmp.diff_cleanupEfficiency(diffs);
+
+            var builder = new StringBuilder("\n");
+
+            builder.AppendLine("- Snapshot");
+            builder.AppendLine("+ Receieved\n\n");
+
+            for (var i = 0; i < diffs.Count; i++)
+            {
+                var diff = diffs[i];
+
+                var lines = GetLines(diff.text);
+
+                switch (diff.operation)
+                {
+                    case Operation.DELETE:
+                        lines.ForEach(l => builder.AppendLine($"-  {l}"));
+                        break;
+                    case Operation.INSERT:
+                        lines.ForEach(l => builder.AppendLine($"+  {l}"));
+                        break;
+                    case Operation.EQUAL:
+                        GetEqualLines(lines, i == 0, i == diffs.Count - 1).ForEach(l => builder.AppendLine(l));
+                        break;
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private static List<string> GetEqualLines(List<string> lines, bool isFirst, bool isLast)
+        {
+            if (isFirst)
+                return lines.Count >= 2 ? lines.Splice(lines.Count - 2, 2) : lines;
+
+            if (isLast)
+                return lines.Count >= 2 ? lines.Splice(0, 2) : lines;
+
+            if (lines.Count <= 4)
+                return lines;
+
+            var firstTwo = lines.Splice(0, 2);
+            var lastTwo = lines.Splice(lines.Count - 2, 2);
+
+            firstTwo.Add("...");
+            firstTwo.AddRange(lastTwo);
+            return firstTwo;
+        }
+
+        private static List<string> GetLines(string text)
+        {
+            var lines = new List<string>();
+            using (var sr = new StringReader(text)) {
+                string line;
+                while ((line = sr.ReadLine()) != null) {
+                    lines.Add(line);
+                }
+            }
+            return lines;
         }
     }
 }

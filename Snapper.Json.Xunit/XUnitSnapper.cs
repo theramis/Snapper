96e0fa79162e40833e15feb1550f1af016b5cd4a
Simar Kalra <TheRamis@users.noreply.github.com>
2018-04-02 01:30:12 +1200

    replace xunit.assert.source dependency with xunit.assert

--- Snapper.Json.Xunit/XUnitSnapper.cs
+++ Snapper.Json.Xunit/XUnitSnapper.cs
@@ -4,7 +4,7 @@ namespace Snapper.Json.Xunit
 {
     public class XUnitSnapper : JsonSnapper
     {
-        internal static XUnitSnapper Create()
+        private static XUnitSnapper Create()
             => new XUnitSnapper(new XUnitAsserter(), new JsonSnapStore(), new XUnitPathResolver(), new XUnitEnvironmentVariableUpdateDecider(), new JsonSnapComparer());
 
         private XUnitSnapper(IAssert asserter, ISnapStore store, IPathResolver resolver, ISnapUpdateDecider snapUpdateDecider, ISnapComparer comparer)
@@ -12,7 +12,17 @@ namespace Snapper.Json.Xunit
         {
         }
 
-        internal void SnapObject(object value)
+        private void SnapObject(object value)
             => Snap(string.Empty, value);
+
+        public static void MatchSnapshot(object value)
+        {
+            Create().SnapObject(value);
+        }
+
+        public static void MatchSnapshot(string snapshotName, object value)
+        {
+            Create().Snap(snapshotName, value);
+        }
     }
 }

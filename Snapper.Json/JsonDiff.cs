b7ef83f1ef439ec5e3ccee03d08c9fdc22fcaf53
fgather <florian.gather@gmail.com>
2019-01-14 20:06:00 +0100

    make json diff class internal

--- Snapper.Json/JsonDiff.cs
+++ Snapper.Json/JsonDiff.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json.Linq;
 
 namespace Snapper.Json
 {
-    public class JsonDiff
+    internal class JsonDiff
     {
         internal static string GetDiffMessage(JToken old, JToken @new)
         {

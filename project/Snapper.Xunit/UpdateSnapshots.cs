d94d1d44e18bbe3253091775b642d3226a5b4868
Simar <theramis@users.noreply.github.com>
2019-04-23 22:49:37 +1200

    Remove Snapper.Xunit

--- project/Snapper.Xunit/UpdateSnapshots.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Snapper.Xunit
-{
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-    public sealed class UpdateSnapshots : Attribute
-    {
-    }
-}

d1b2ae1b749715c079dc30eb89733dd3f1cd4c19
Taylor Kimmett <taylor@cognitoforms.com>
2019-10-31 11:46:50 -0400

    remove MSTest project and move tests to shared project

---
+++ project/Tests/Snapper.TestFrameworkSupport.Tests/MSTestFrameworkSupportTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Snapper.TestFrameworkSupport.Tests
+{
+    [TestClass]
+    public class MSTestFrameworkSupportTests
+    {
+        [TestMethod]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void MSTestMethod()
+        {
+            "value".ShouldMatchInlineSnapshot("value");
+        }
+
+        [DataTestMethod]
+        [DataRow("Data")]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void MSTestDataTestMethod(string value)
+        {
+            value.ShouldMatchInlineSnapshot(value);
+        }
+    }
+}

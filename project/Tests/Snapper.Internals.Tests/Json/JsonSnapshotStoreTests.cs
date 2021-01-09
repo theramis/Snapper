92edbac6d19a6b7348733cd45fec0864f28445ea
simi <simon@artify.email>
2021-01-10 00:27:18 +0100

    Cleanup tests

--- project/Tests/Snapper.Internals.Tests/Json/JsonSnapshotStoreTests.cs
+++ project/Tests/Snapper.Internals.Tests/Json/JsonSnapshotStoreTests.cs
@@ -10,12 +10,16 @@ namespace Snapper.Internals.Tests.Json
 {
     public class JsonSnapshotStoreTests
     {
+        public enum MyTestEnum
+        {
+            FirstValue = 0,
+            SecondValue = 1,
+            LastValue = 99
+        }
+
         private readonly JsonSnapshotStore _snapshotStore;
 
-        public JsonSnapshotStoreTests()
-        {
-            _snapshotStore = new JsonSnapshotStore();
-        }
+        public JsonSnapshotStoreTests() => _snapshotStore = new JsonSnapshotStore();
 
         [Fact]
         public void LoadingNonExistingSnapshot()
@@ -24,19 +28,13 @@ namespace Snapper.Internals.Tests.Json
 
             snapshot.Should().BeNull();
         }
-        
 
         [Fact]
         public void StoreAndLoadSnapshotWithCustomizedSettings()
         {
             var snapshotId = new SnapshotId(".", nameof(JsonSnapshotStoreTests), nameof(StoreAndLoadSnapshotWithCustomizedSettings));
-            var fancyState = new
-            {
-                MyProp = MyTestEnum.FirstValue,
-                OtherProp = MyTestEnum.LastValue,
-                SomeString = "Nice ..."
-            };
-            
+            var fancyState = new {MyProp = MyTestEnum.FirstValue, OtherProp = MyTestEnum.LastValue, SomeString = "Nice ..."};
+
             _snapshotStore.StoreSnapshot(snapshotId, fancyState);
 
             var result = _snapshotStore.GetSnapshot(snapshotId);
@@ -45,25 +43,11 @@ namespace Snapper.Internals.Tests.Json
             expected.Value<string>("MyProp").Should().Be(nameof(MyTestEnum.FirstValue));
             expected.Value<string>("OtherProp").Should().Be(nameof(MyTestEnum.LastValue));
             result.Should().BeEquivalentTo(expected);
-            
         }
 
-
-        public enum MyTestEnum
-        {
-            FirstValue= 0,
-            SecondValue = 1,
-            LastValue = 99
-        }
         public class CustomSnapshotSerializerSettings : ICustomSnapshotSerializerSettings
         {
-            public JsonSerializerSettings JsonSerializerSettings => new()
-            {
-                Converters = new List<JsonConverter>
-                {
-                    new StringEnumConverter()
-                }
-            };
+            public JsonSerializerSettings JsonSerializerSettings => new() {Converters = new List<JsonConverter> {new StringEnumConverter()}};
         }
     }
 }

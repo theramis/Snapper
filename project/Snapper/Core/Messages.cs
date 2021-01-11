8cd164318ad90cdb9998bc0d7349024063d6dbb8
Simar <theramis@users.noreply.github.com>
2021-01-11 19:06:33 +1300

    Updated Snapper to create snapshot if none exists (#71)
    * Updated Snapper to create snapshot if none exists
    
    * Update test projects to .net core 3.1
    
    * Bump appveyor image to VS 2019
    
    * Fix bug with updating snapshots in CI env
    
    * Update docs to mention auto creating of initial snapshot
    
    * Update test projects to use .net 452 so that the latest xunit works
    
    * Remove appveyor logging as its not possible to make it work perfectly
    
    For NUnit tests to be logged in appveyor you need to the use the AppVeyor logger. 
    Unfortunately since we run the same test twice, once for .net core and once for .net framework the AppVeyor logger doesn't detect the difference and only logs the test once, which results in incorrect numbers. 
    
    Removed AppVeyor  logger fully and not going to rely on the number in CI.

--- project/Snapper/Core/Messages.cs
+++ project/Snapper/Core/Messages.cs
@@ -1,6 +1,5 @@
-using System;
+﻿using System;
 using System.Text;
-using Snapper.Attributes;
 using Snapper.Json;
 
 namespace Snapper.Core
@@ -12,12 +11,9 @@ namespace Snapper.Core
             switch (result.Status)
             {
                 case SnapResultStatus.SnapshotDoesNotExist:
-                    var attributeName = nameof(UpdateSnapshotsAttribute).Replace("Attribute", string.Empty);
                     var message = new StringBuilder();
                     message.AppendLine($"A snapshot does not exist.{Environment.NewLine}");
-                    message.AppendLine($"Apply the [{attributeName}] attribute on the " +
-                                       $"test method or class and then run the test again to " +
-                                       $"create a snapshot.");
+                    message.AppendLine("Run the test outside of a CI environment to create a snapshot.");
                     return message.ToString();
                 case SnapResultStatus.SnapshotsMatch:
                     return "Snapshots Match.";
